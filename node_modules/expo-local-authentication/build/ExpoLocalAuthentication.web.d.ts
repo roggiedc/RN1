04bc54bc99876a5af99c79759277d38b0ddac5b4
Jason David <82432588+Jaycd598@users.noreply.github.com>
2022-08-19 16:57:05 +0800

    update

--- node_modules/expo-local-authentication/build/ExpoLocalAuthentication.web.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthenticationType, SecurityLevel } from './LocalAuthentication.types';
-declare const _default: {
-    readonly name: string;
-    hasHardwareAsync(): Promise<boolean>;
-    isEnrolledAsync(): Promise<boolean>;
-    getEnrolledLevelAsync(): Promise<SecurityLevel>;
-    supportedAuthenticationTypesAsync(): Promise<AuthenticationType[]>;
-};
-export default _default;
